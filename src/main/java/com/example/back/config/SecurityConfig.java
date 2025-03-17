3f28bedec9a7eaa487ee59943c7bded692754b5d
DESKTOP-6279PG5\Tae <mm0307a@gmail.com>
2025-03-18 04:52:05 +0900

    서치및 구글 플에시ê·스 API 수정

--- src/main/java/com/example/back/config/SecurityConfig.java
+++ src/main/java/com/example/back/config/SecurityConfig.java
@@ -24,6 +24,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration //spring 설정 클래스임을 나타냄
@@ -32,85 +35,94 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor // final로 선언된 필드를 자동으로 생성자를 통해 주입
 public class SecurityConfig {
 
-	private final JwtAuthenticationFilter jwtAuthenticationFilter;
-	private final CorsFilter corsFilter;
-	private final UserService userService;
-	private final UserDao userDao;
-	private final JWTService jwtService;
-	private final AuthenticationConfiguration authenticationConfiguration;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final CorsFilter corsFilter;
+  private final UserService userService;
+  private final UserDao userDao;
+  private final JWTService jwtService;
+  private final AuthenticationConfiguration authenticationConfiguration;
 
-	// ✅ AuthenticationManager를 Bean으로 등록
-	// Spring Security에서 인증을 관리하는 AuthenticationManager를 Bean으로 등록
-	@Bean
-	public AuthenticationManager authenticationManager() throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
-	}
+  // ✅ AuthenticationManager를 Bean으로 등록
+  // Spring Security에서 인증을 관리하는 AuthenticationManager를 Bean으로 등록
+  @Bean
+  public AuthenticationManager authenticationManager() throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
-	/* AuthenticationService를 Bean으로 등록하여 의존성 주입을 가능하게 함 */
-	@Bean
-	public AuthenticationService authenticationService(AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
-		return new AuthenticationService(userDao, jwtService, authenticationManager, passwordEncoder);
-	}
+  /* AuthenticationService를 Bean으로 등록하여 의존성 주입을 가능하게 함 */
+  @Bean
+  public AuthenticationService authenticationService(AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+    return new AuthenticationService(userDao, jwtService, authenticationManager, passwordEncoder);
+  }
 
-	/* oauth2 로그인을 성공시 실행될 핸들러 bean으로 등록, google, naver, kakao jwt를 발급하는 역할 */
-	@Bean
-	public OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler(AuthenticationService authenticationService) {
-		return new OAuth2LoginSuccessHandler(authenticationService);
-	}
+  /* oauth2 로그인을 성공시 실행될 핸들러 bean으로 등록, google, naver, kakao jwt를 발급하는 역할 */
+  @Bean
+  public OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler(AuthenticationService authenticationService) {
+    return new OAuth2LoginSuccessHandler(authenticationService);
+  }
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationService authenticationService) throws Exception {
-		http
-				.cors(Customizer.withDefaults()) //cors 설정
-				.addFilter(corsFilter) // cors 필터 추가
-				.csrf(csrf -> csrf.disable()) // csrf 보안 비활성화 (jwt 이므로 필요가 없음)
-				.authorizeHttpRequests(requests -> requests
-						.requestMatchers("/api/**").permitAll()
-						.requestMatchers("/api/login").permitAll()
-						.requestMatchers("/oauth2/**").permitAll()
-						.requestMatchers("/api/search/**").permitAll()
-						.requestMatchers("/api/places/**").permitAll()
-						.requestMatchers("/api/check").authenticated() // 🔥 인증된 사용자만 접근 가능
-						.requestMatchers("/oauth2/authorization/**").permitAll() // ✅ OAuth2 로그인 엔드포인트 허용
-						.requestMatchers("/oauth/naver/callback").permitAll()
-						.requestMatchers("/oauth/google/callback").permitAll()
-						.requestMatchers("/error").permitAll()
-						.anyRequest().authenticated())
+  // CORS 설정 추가
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedOrigin("http://localhost:3000"); // 허용할 프론트엔드 URL 설정
+    corsConfiguration.addAllowedMethod("*"); // 모든 HTTP 메서드(GET, POST, PUT, DELETE 등) 허용
+    corsConfiguration.addAllowedHeader("*"); // 모든 헤더 허용
+    corsConfiguration.setAllowCredentials(true); // 자격 증명(Cookie 등) 허용
 
-				.formLogin(form -> form.disable())  // ✅ Security에서 기본 로그인 페이지 제공 제거
-				.httpBasic(httpBasic -> httpBasic.disable()) // ✅ HTTP Basic 인증 제거
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/api/**", corsConfiguration); // /api/** 경로에 CORS 적용
 
-				.oauth2Login(oauth2 -> oauth2
-						.authorizationEndpoint(endpoint -> endpoint.baseUri("/oauth2/authorization"))
-						.redirectionEndpoint(endpoint -> endpoint.baseUri("/login/oauth2/code/*"))
-						.successHandler(oAuth2LoginSuccessHandler(authenticationService)) // ✅ 로그인 성공 후 처리
-						.failureHandler((request, response, exception) -> { // ✅ 로그인 실패 시 `/login?error` 방지
-							log.error("OAuth2 로그인 실패: {}", exception.getMessage());
+    return source;
+  }
 
-							String referer = request.getHeader("Referer");
-							if (referer != null && referer.contains("naver")) {
-								response.sendRedirect("/oauth2/authorization/naver"); // 네이버 로그인 실패시 재시도
-							} else {
-								response.sendRedirect("/oauth2/authorization/google"); //구글 로그인 실패 시 재시도
-							}
-						})
-				)
-				.sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authenticationProvider(authenticationProvider())
-				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		return http.build();
-	}
 
-	@Bean
-	public AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(userService.userDetailsService());
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
-		return authenticationProvider;
-	}
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationService authenticationService) throws Exception {
+    http.cors(Customizer.withDefaults()) //cors 설정
+        .addFilter(corsFilter) // cors 필터 추가
+        .csrf(csrf -> csrf.disable()) // csrf 보안 비활성화 (jwt 이므로 필요가 없음)
+        .authorizeHttpRequests(requests -> requests.requestMatchers("/api/**").permitAll() // 모든 API 경로 허용
+            .requestMatchers("/api/login").permitAll() // 로그인 경로 허용
+            .requestMatchers("/oauth2/**").permitAll() // OAuth2 관련 경로 허용
+            .requestMatchers("/api/search/**").permitAll() // 검색 API 경로 허용
+            .requestMatchers("/api/places/**").permitAll() // Places API 경로 허용
+            .requestMatchers("/api/check").authenticated() // 🔥 인증된 사용자만 접근 가능
+            .requestMatchers("/oauth2/authorization/**").permitAll() // ✅ OAuth2 로그인 엔드포인트 허용
+            .requestMatchers("/oauth/naver/callback").permitAll() // 네이버 콜백 허용
+            .requestMatchers("/oauth/google/callback").permitAll() // 구글 콜백 허용
+            .requestMatchers("/error").permitAll() // 에러 경로 허용
+            .anyRequest().authenticated()) // 그 외 모든 요청은 인증 필요
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+        .formLogin(form -> form.disable())  // ✅ Security에서 기본 로그인 페이지 제공 제거
+        .httpBasic(httpBasic -> httpBasic.disable()) // ✅ HTTP Basic 인증 제거
+
+        .oauth2Login(oauth2 -> oauth2.authorizationEndpoint(endpoint -> endpoint.baseUri("/oauth2/authorization")).redirectionEndpoint(endpoint -> endpoint.baseUri("/login/oauth2/code/*")).successHandler(oAuth2LoginSuccessHandler(authenticationService)) // ✅ 로그인 성공 후 처리
+            .failureHandler((request, response, exception) -> { // ✅ 로그인 실패 시 `/login?error` 방지
+              log.error("OAuth2 로그인 실패: {}", exception.getMessage());
+
+              String referer = request.getHeader("Referer");
+              if (referer != null && referer.contains("naver")) {
+                response.sendRedirect("/oauth2/authorization/naver"); // 네이버 로그인 실패시 재시도
+              } else {
+                response.sendRedirect("/oauth2/authorization/google"); //구글 로그인 실패 시 재시도
+              }
+            })).sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 상태 비저장 세션 정책
+        .authenticationProvider(authenticationProvider()) // 인증 공급자 설정
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 인증 필터 추가
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userService.userDetailsService());
+    authenticationProvider.setPasswordEncoder(passwordEncoder());
+    return authenticationProvider;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
